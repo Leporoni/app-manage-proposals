048b1683ee73dada1d7d32e5d19588b5dbe8aba1
Leporoni <leporoni@hotmail.com>
2019-11-22 16:50:07 -0300

    first commit

---
+++ src/main/java/com/leporonitech/appmanageproposals/AppManageProposalsApplication.java
@@ -0,0 +1,13 @@
+package com.leporonitech.appmanageproposals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppManageProposalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppManageProposalsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leporonitech/appmanageproposals/AppManageProposalsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leporonitech.appmanageproposals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppManageProposalsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
