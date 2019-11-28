cef13426289d1c5f03284b4dd570025cf11839e4
Leporoni <leporoni@hotmail.com>
2019-11-28 13:17:06 -0300

    add delete em update on clienteController

--- src/main/java/com/leporonitech/appmanageproposals/controller/ClienteController.java
+++ src/main/java/com/leporonitech/appmanageproposals/controller/ClienteController.java
@@ -1,6 +1,7 @@
 package com.leporonitech.appmanageproposals.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import javax.validation.Valid;
 
@@ -10,12 +11,12 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.leporonitech.appmanageproposals.model.Cliente;
 import com.leporonitech.appmanageproposals.repository.ClienteRepository;
-
 
 @RestController
 public class ClienteController {
@@ -36,11 +37,26 @@ public class ClienteController {
 		this.clienteRepository.save(cliente);
 		return ResponseEntity.status(201).build();
 	}
-	
-	@DeleteMapping("{id}")
-	public ResponseEntity<Void> excluirCliente(@PathVariable Long id){
-		
+
+	@DeleteMapping("/excluir/{id}")
+	public ResponseEntity<Void> excluirCliente(@PathVariable Long id) {
+
 		clienteRepository.deleteById(id);
 		return ResponseEntity.accepted().build();
+	}
+
+	@PutMapping("/alterar/{id}")
+	public ResponseEntity<Cliente> alterarCliente(@PathVariable("id") Long id, @RequestBody Cliente cliente) {
+
+		Optional<Cliente> clienteExistente = clienteRepository.findById(id);
+
+		if (!clienteExistente.isPresent())
+			return ResponseEntity.notFound().build();
+
+		cliente.setId(id);
+
+		clienteRepository.save(cliente);
+
+		return ResponseEntity.status(200).build();
 	}
 }
